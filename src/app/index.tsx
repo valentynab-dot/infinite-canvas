61d54deccaca3c444ac29538e1c52e053f08cfd8
valentynab-dot <valentynab@wix.com>
2026-06-20 00:34:35 +0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,38 @@ import type { MediaItem } from "~/src/infinite-canvas/types";
 import { PageLoader } from "~/src/loader";
 
 export function App() {
-  const [media] = React.useState<MediaItem[]>(manifest);
+  import * as React from "react";
+import { Frame } from "~/src/frame";
+import { InfiniteCanvas } from "~/src/infinite-canvas";
+import type { MediaItem } from "~/src/infinite-canvas/types";
+import { PageLoader } from "~/src/loader";
+
+export function App() {
+  const [media, setMedia] = React.useState<MediaItem[]>([]);
+  const [textureProgress, setTextureProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data?.type === "LOAD_PROJECTS") {
+        setMedia(event.data.projects);
+      }
+    };
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
+  if (!media.length) {
+    return <PageLoader progress={0} />;
+  }
+
+  return (
+    <>
+      <Frame />
+      <PageLoader progress={textureProgress} />
+      <InfiniteCanvas media={media} onTextureProgress={setTextureProgress} />
+    </>
+  );
+}
   const [textureProgress, setTextureProgress] = React.useState(0);
 
   if (!media.length) {
