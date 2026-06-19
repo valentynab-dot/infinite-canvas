6c058efab91f4bba6f672228ca26e4b42b958410
valentynab-dot <valentynab@wix.com>
2026-06-20 00:36:38 +0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,4 @@
 import * as React from "react";
-import manifest from "~/src/artworks/manifest.json";
-import { Frame } from "~/src/frame";
-import { InfiniteCanvas } from "~/src/infinite-canvas";
-import type { MediaItem } from "~/src/infinite-canvas/types";
-import { PageLoader } from "~/src/loader";
-
-export function App() {
-  import * as React from "react";
 import { Frame } from "~/src/frame";
 import { InfiniteCanvas } from "~/src/infinite-canvas";
 import type { MediaItem } from "~/src/infinite-canvas/types";
@@ -25,20 +17,6 @@ export function App() {
     window.addEventListener("message", handleMessage);
     return () => window.removeEventListener("message", handleMessage);
   }, []);
-
-  if (!media.length) {
-    return <PageLoader progress={0} />;
-  }
-
-  return (
-    <>
-      <Frame />
-      <PageLoader progress={textureProgress} />
-      <InfiniteCanvas media={media} onTextureProgress={setTextureProgress} />
-    </>
-  );
-}
-  const [textureProgress, setTextureProgress] = React.useState(0);
 
   if (!media.length) {
     return <PageLoader progress={0} />;
